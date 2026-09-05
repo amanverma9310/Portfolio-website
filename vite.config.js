7e67cc5c85845b480581b821db10f33c97a47b98
Aman verma <amanverm9310@gmail.com>
2026-09-05 09:42:02 +0530

    save

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio-website',
+  plugins: [react()],
+    base: '/Portfolio-website',
 })
